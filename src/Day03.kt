c9b31d96e8593e30b7ce4fc1dab36ca64ca33aa3
Miguel Del Corso <migueldelcorso@gmail.com>
2025-03-30 10:24:16 -0300

    Improved Day03

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,40 +1,48 @@
 data class House(val r: Int = 0, val c: Int = 0)
+enum class Direction {
+    LEFT, RIGHT, UP, DOWN
+}
 
 fun main() {
-    fun housesVisited(directions: List<Char>): Set<House> {
+    fun housesVisited(directions: List<Direction>): Set<House> {
         var currentHouse = House()
-        val houses = mutableSetOf(currentHouse)
-        for (direction in directions) {
-            when (direction) {
-                '>' -> currentHouse = House(currentHouse.r, currentHouse.c + 1)
-                '<' -> currentHouse = House(currentHouse.r, currentHouse.c - 1)
-                '^' -> currentHouse = House(currentHouse.r - 1, currentHouse.c)
-                'v' -> currentHouse = House(currentHouse.r + 1, currentHouse.c)
+        return buildSet {
+            add(currentHouse)
+            for (direction in directions) {
+                currentHouse = when (direction) {
+                    Direction.RIGHT -> currentHouse.copy(c = currentHouse.c + 1)
+                    Direction.LEFT -> currentHouse.copy(c = currentHouse.c - 1)
+                    Direction.UP -> currentHouse.copy(r = currentHouse.r - 1)
+                    Direction.DOWN -> currentHouse.copy(r = currentHouse.r + 1)
+                }
+                add(currentHouse)
             }
-            houses.add(currentHouse)
         }
-        return houses
     }
 
-    fun part1(directions: List<Char>): Int {
-        return housesVisited(directions).size
+    fun parseInput(input: List<Char>): List<Direction> {
+        return buildList {
+            for (direction in input) {
+                when (direction) {
+                    '>' -> add(Direction.RIGHT)
+                    '<' -> add(Direction.LEFT)
+                    '^' -> add(Direction.UP)
+                    'v' -> add(Direction.DOWN)
+                }
+            }
+        }
     }
 
+    val filterInput = { directions: List<Direction>, odd: Boolean -> directions.filterIndexed { i, _ -> i % 2 == if (odd) 0 else 1 } }
 
-    fun filterInput(input: List<Char>, odd: Boolean = true): List<Char> {
-        return input.filterIndexed { i, _ -> i % 2 == if (odd) 0 else 1 }
-    }
+    val part1 = { directions: List<Direction> -> housesVisited(directions).size }
+    val part2 = { directions: List<Direction> -> (housesVisited(filterInput(directions, false)) + housesVisited(filterInput(directions, true))).size }
 
-    fun part2(directions: List<Char>): Int {
-        val s = housesVisited(filterInput(directions, false)) + housesVisited(filterInput(directions))
-        return s.size
-    }
+    val testInput = parseInput(readOneLineInput("Day03_test"))
+    check(part1(testInput) == 2)
+    check(part2(testInput) == 11)
 
-    val testInput = readOneLineInput("Day03_test")
-    part1(testInput)
-    part2(testInput)
-
-    val input = readOneLineInput("Day03")
-    part1(input).println()
-    part2(input).println()
+    val input = parseInput(readOneLineInput("Day03"))
+    check(part1(input) == 2081)
+    check(part2(input) == 2341)
 }
