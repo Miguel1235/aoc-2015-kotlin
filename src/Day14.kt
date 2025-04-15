2c8387e2a8aa00e2553192b8149d809ce3acbe95
Miguel Del Corso <migueldelcorso@gmail.com>
2025-04-15 11:41:22 -0300

    Finished part1 of the day 14

---
+++ src/Day14.kt
@@ -0,0 +1,36 @@
+fun main() {
+
+    data class Racer(val name: String, val speed: Int, val flyingTime: Int, val restingTime: Int, var currentDistance: Int = 0, var currentTime: Int = 0) {
+        fun move() {
+            val toMod = flyingTime + restingTime
+            currentDistance += if(currentTime % toMod < flyingTime) speed else 0
+            currentTime++
+        }
+    }
+
+    fun obtainRacers(input: List<String>): List<Racer> {
+        val regex = Regex("""^(\w+) can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+) seconds\.""")
+        return buildList {
+            for(line in input) {
+                val (_, name ,v, t, r) = regex.find(line)!!.groupValues
+                add(Racer(name, v.toInt(), t.toInt(), r.toInt()))
+            }
+        }
+    }
+
+    fun part1(input: List<String>, seconds: Int = 1000): Int {
+        val racers = obtainRacers(input)
+
+        repeat(seconds) {
+            racers.forEach { it.move() }
+        }
+
+        return racers.maxOf { it.currentDistance }
+    }
+
+    val testInput = readInput("Day14_test")
+    check(part1(testInput) == 1120)
+
+    val input = readInput("Day14")
+    check(part1(input, 2503) == 2696)
+}
