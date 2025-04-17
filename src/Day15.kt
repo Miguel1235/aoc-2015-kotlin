6b4232bec1233d2dd91dd185ef8cce079e6c280d
Miguel Del Corso <migueldelcorso@gmail.com>
2025-04-17 10:10:43 -0300

    Made some improves on the day15

--- src/Day15.kt
+++ src/Day15.kt
@@ -2,7 +2,6 @@ import kotlin.math.max
 
 fun main() {
     data class Ingredient(
-        val name: String,
         val capacity: Int,
         val durability: Int,
         val flavor: Int,
@@ -12,11 +11,11 @@ fun main() {
 
     fun obtainIngredients(input: List<String>): List<Ingredient> {
         val ingredientRegex =
-            Regex("""(\w+): capacity (-?\d+), durability (-?\d+), flavor (-?\d+), texture (-?\d+), calories (-?\d+)""")
+            Regex("""capacity (-?\d+), durability (-?\d+), flavor (-?\d+), texture (-?\d+), calories (-?\d+)""")
         return buildList {
             input.map {
-                val r = ingredientRegex.find(it)!!.groupValues.drop(1)
-                add(Ingredient(r[0], r[1].toInt(), r[2].toInt(), r[3].toInt(), r[4].toInt(), r[5].toInt()))
+                val (cap, dur, flav, text, cal) = ingredientRegex.find(it)!!.groupValues.drop(1)
+                add(Ingredient(cap.toInt(), dur.toInt(), flav.toInt(), text.toInt(), cal.toInt()))
             }
         }
     }
@@ -31,46 +30,44 @@ fun main() {
                 results.add(currentCombination.toList())
                 return
             }
-
             for (value in 0..remainingSum) {
                 currentCombination[index] = value
                 generateCombinationsRecursive(index + 1, remainingSum - value)
             }
         }
-
         generateCombinationsRecursive(0, targetSum)
         return results
     }
 
 
-    fun part1(input: List<String>, isPart2: Boolean = false): Long {
+    fun part1(input: List<String>, isPart2: Boolean = false): Int {
         val ingredients = obtainIngredients(input)
         val combinations = generateCombinations(ingredients.size, 100)
 
         return combinations.maxOf { combination ->
-            val cap = max(ingredients.indices.sumOf { ingredients[it].capacity.toLong() * combination[it].toLong() }, 0L)
-            val dur = max(ingredients.indices.sumOf { ingredients[it].durability.toLong() * combination[it].toLong() }, 0L)
-            val flav = max(ingredients.indices.sumOf { ingredients[it].flavor.toLong() * combination[it].toLong() }, 0L)
-            val text = max(ingredients.indices.sumOf { ingredients[it].texture.toLong() * combination[it].toLong() }, 0L)
+            val cap = max(ingredients.indices.sumOf { ingredients[it].capacity * combination[it] }, 0)
+            val dur = max(ingredients.indices.sumOf { ingredients[it].durability * combination[it] }, 0)
+            val flav = max(ingredients.indices.sumOf { ingredients[it].flavor * combination[it] }, 0)
+            val text = max(ingredients.indices.sumOf { ingredients[it].texture * combination[it] }, 0)
 
-
-            if(isPart2) {
-                if(ingredients.indices.sumOf { ingredients[it].calories.toLong() * combination[it].toLong() } == 500L) {
-                    cap * dur * flav * text
-                } else {
-                    0
+            when(isPart2) {
+                true -> {
+                    if (ingredients.indices.sumOf { ingredients[it].calories * combination[it] } == 500) {
+                        cap * dur * flav * text
+                    } else {
+                        0
+                    }
                 }
-            } else {
-                cap * dur * flav * text
+                false -> cap * dur * flav * text
             }
         }
     }
 
     val testInput = readInput("Day15_test")
-    check(part1(testInput) == 62842880L)
-    check(part1(testInput, true) == 57600000L)
+    check(part1(testInput) == 62842880)
+    check(part1(testInput, true) == 57600000)
 
     val input = readInput("Day15")
-    check(part1(input) == 21367368L)
-    check(part1(input, true) == 1766400L)
+    check(part1(input) == 21367368)
+    check(part1(input, true) == 1766400)
 }
