375b3b5cfd8e8d22b221738be37cc67863bd8fb9
Miguel Del Corso <migueldelcorso@gmail.com>
2025-04-19 10:37:50 -0300

    Finished day 17 on both days

--- src/Day17.kt
+++ src/Day17.kt
@@ -1,20 +1,29 @@
 fun main() {
-    fun part1(input: List<String>, liters2Fill: Int): Int {
-        val intInput = input.map { it.toInt() }
 
+    fun obtainAllCombinations(input: List<Int>): List<List<Int>> {
         return buildList {
-            for (i in 1..intInput.size) {
-                add(combinations(intInput, i))
+            for(i in 1..input.size) {
+                add(combinations(input, i))
             }
         }.flatten()
-            .count {
-                it.sum() == liters2Fill
-            }
     }
 
-    val testInput = readInput("Day17_test")
-    check(part1(testInput, 25) == 4)
+    fun part1(input: List<Int>, liters2Fill: Int): Int {
+        return obtainAllCombinations(input).count { it.sum() == liters2Fill }
+    }
 
-    val input = readInput("Day17")
+    fun part2(input: List<Int>, liters2Fill: Int): Int {
+        val combinations = obtainAllCombinations(input)
+
+        val minContainers = combinations.find { it.sum() == liters2Fill }!!.size
+        return combinations.count { it.size == minContainers && it.sum() == liters2Fill }
+    }
+
+    val testInput = readInput("Day17_test").map { it.toInt() }
+    check(part1(testInput, 25) == 4)
+    check(part2(testInput, 25) == 3)
+
+    val input = readInput("Day17").map { it.toInt() }
     check(part1(input, 150) == 1638)
+    check(part2(input, 150) == 17)
 }
